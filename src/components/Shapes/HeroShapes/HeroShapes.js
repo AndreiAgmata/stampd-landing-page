e5811cdfd4b4cb1e164f0fc69cde5cae44dc18f7
AndreiAgmata <agmataa1124@gmail.com>
2024-03-04 03:57:03 -0500

    Added more animations to hero

--- src/components/Shapes/HeroShapes/HeroShapes.js
+++ src/components/Shapes/HeroShapes/HeroShapes.js
@@ -1,11 +1,45 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import "./HeroShapes.scss";
+import gsap from "gsap";
 
 function HeroShapes() {
+  let shapeDark = useRef();
+  let shapeLight = useRef();
+
+  useEffect(() => {
+    const ctx = new gsap.context(() => {
+      const tl = new gsap.timeline();
+
+      tl.from(shapeDark, {
+        y: -1000,
+        x: 1000,
+        duration: 1,
+        ease: "power3.out",
+      }).from(
+        shapeLight,
+        {
+          y: -1000,
+          x: 1000,
+          duration: 1,
+          ease: "power3.out",
+        },
+        "<0.25"
+      );
+    });
+
+    return () => ctx.revert();
+  });
   return (
     <div className="shapes">
-      <div className="shape-dark position-absolute rounded-4"></div>
-      <div className="shape-light position-absolute rounded-4"></div>
+      <div
+        className="shape-dark position-absolute rounded-4"
+        ref={(el) => (shapeDark = el)}
+      ></div>
+      <div
+        className="shape-light position-absolute rounded-4"
+        ref={(el) => (shapeLight = el)}
+      ></div>
     </div>
   );
 }
