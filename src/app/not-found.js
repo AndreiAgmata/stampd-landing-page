64aa3f623b9bfa67b210cd1d400a4ce323b88f66
Andrei Luigi Agmata <agmataa1124@gmail.com>
2024-03-24 20:47:15 -0400

    Added not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,28 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+function Notfound() {
+  const router = useRouter();
+
+  return (
+    <section
+      className="not-found d-flex flex-column justify-content-center align-items-center h-100"
+      style={{ marginTop: "-4rem" }}
+    >
+      <h1 className="text-primary fw-bold" style={{ fontSize: "7rem" }}>
+        404
+      </h1>
+      <p className="fw-medium">Page Not Found or Unavailable</p>
+      <button
+        type="button"
+        className="btn btn-primary mt-5"
+        onClick={() => router.push("/")}
+      >
+        Go to Home
+      </button>
+    </section>
+  );
+}
+
+export default Notfound;
--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main>
       <HeroShapes />
-      <Navbar />
+
       <Hero />
       <Overview />
       <Steps />
